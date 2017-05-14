b75f904c0f47061dd9ce9606216565e2ad55ee39
Fahad-Alhajjaj <Fahad.Alhajjaj@gmail.com>
2017-05-14 20:50:52 +0300

    p5

--- Intro_to_Machine_Learning/ud120-projects-master/datasets_questions/explore_enron_data.py
+++ Intro_to_Machine_Learning/ud120-projects-master/datasets_questions/explore_enron_data.py
@@ -1,22 +1,43 @@
-#!/usr/bin/python
-
-""" 
-    Starter code for exploring the Enron dataset (emails + finances);
-    loads up the dataset (pickled dict of dicts).
-
-    The dataset has the form:
-    enron_data["LASTNAME FIRSTNAME MIDDLEINITIAL"] = { features_dict }
-
-    {features_dict} is a dictionary of features associated with that person.
-    You should explore features_dict as part of the mini-project,
-    but here's an example to get you started:
-
-    enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
-"""
-
 import pickle
+from math import isnan
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+import sys
+sys.path.append('../tools')
+from feature_format import *
 
+a = 0
+b = 0
+for item in enron_data:
+	for k, v in enron_data[item].items():
+		if k == 'total_payments':
+			if v == 'NaN':
+				a+=1
+			else:
+				b+=1
+
+print a, ' | ', b
+'''
+salary
+to_messages
+deferral_payments
+total_payments
+exercised_stock_options
+bonus
+restricted_stock
+shared_receipt_with_poi
+restricted_stock_deferred
+total_stock_value
+expenses
+loan_advances
+from_messages
+other
+from_this_person_to_poi
+poi
+director_fees
+deferred_income
+long_term_incentive
+email_address
+from_poi_to_this_person
+'''
