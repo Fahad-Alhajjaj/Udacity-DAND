3ddb45ac599f38ee195ac9fdcdfc3a3cc1757299
Fahad-Alhajjaj <fahad.alhajjaj@gmail.com>
2017-05-17 23:37:11 +0300

    p5

--- Intro_to_Machine_Learning/ud120-projects-master/validation/validate_poi.py
+++ Intro_to_Machine_Learning/ud120-projects-master/validation/validate_poi.py
@@ -26,7 +26,17 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
-
 ### it's all yours from here forward!  
+from sklearn import cross_validation
+from sklearn import datasets
 
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+clf = tree.DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+acc = accuracy_score(pred, y_test)
+
+print acc
