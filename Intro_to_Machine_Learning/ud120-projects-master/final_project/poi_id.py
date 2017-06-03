af496a2901baf767a95433fb265b9ba531c1ddc8
Fahad-Alhajjaj <Fahad.Alhajjaj@gmail.com>
2017-06-03 07:08:43 +0300

    p5 - project

--- Intro_to_Machine_Learning/ud120-projects-master/final_project/poi_id.py
+++ Intro_to_Machine_Learning/ud120-projects-master/final_project/poi_id.py
@@ -18,78 +18,160 @@ from sklearn.grid_search import GridSearchCV
 ### The first feature must be "poi".
 				
 features_list = ['poi',
-				'salary',
-				#'deferral_payments',
-				#'total_payments',
-				#'loan_advances',
+				'salary', #~
+				'deferral_payments',
+				'total_payments',
+				#'loan_advances', ####
 				#'bonus',
 				#'restricted_stock_deferred',
-				'deferred_income',
-				'total_stock_value',
+				'deferred_income', #~
+				'total_stock_value', #~
 				'expenses',
 				'exercised_stock_options',
-				#'other',
-				#'long_term_incentive',
-				#'restricted_stock',
-				#'director_fees',
+				'other', ####
+				#'long_term_incentive', ####
+				'restricted_stock',
+				'director_fees',
 				'to_messages',
-				'from_poi_to_this_person',
+				'from_poi_to_this_person', #~
 				'from_messages',
 				'from_this_person_to_poi',
 				'shared_receipt_with_poi',
+				'bonus/salary',
+				'director_fees/deferral_payments',
+				'to_messages/deferral_payments', #~
+				#'from_messages/deferral_payments',
+				'bonus/total_payments',
+				#'shared_receipt_with_poi/total_payments', #~
+				'exercised_stock_options/total_stock_value',
+				'restricted_stock/total_stock_value',
+				'from_this_person_to_poi/to_messages', #~
+				'shared_receipt_with_poi/to_messages',
 				] # You will need to use more features
 
+'''
+~salary
+deferral_payments
+total_payments
+bonus
+restricted_stock_deferred
+~deferred_income
+~total_stock_value
+expenses
+exercised_stock_options
+restricted_stock
+director_fees
+to_messages
+~from_poi_to_this_person
+from_messages
+from_this_person_to_poi
+shared_receipt_with_poi
+'''
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 data_dict.pop('TOTAL', 0)
-'''
-emails = []
-i=0
-for key in data_dict.keys():
-	if i == 0:
-		pprint.pprint(data_dict[key])
-		i+=1
-	emails.append(data_dict[key]['email_address'])
 
-emails_dict = count_emails(emails)
-
-#pprint.pprint(emails_dict)
 ### Task 2: Remove outliers
-data_list = {}
-for key in data_dict.keys():
-	for k, v in data_dict[key].items():
-		if k not in features_list[1:] or v == 'NaN':
-			continue
-		if k in data_list:
-			data_list[k].append(v)
-		else:
-			data_list[k] = []
-			data_list[k].append(v)
-
-counter = 0
-for val in features_list[1:6]:
-	for i in range(0,2):
+keys_outliers = [#'SKILLING JEFFREY K',
+				#'LAY KENNETH L',
+				'FREVERT MARK A',
+				'BHATNAGAR SANJAY',
+				'LAVORATO JOHN J',
+				#'DELAINEY DAVID W',
+				#'BELDEN TIMOTHY N',
+				'MARTIN AMANDA K',
+				'DERRICK JR. JAMES V'
+				#'PICKERING MARK R'
+				]
+for val in keys_outliers:
+	del data_dict[val]
+'''
+for val in features_list[1:]:
+	print 'features: ', val
+	for i in range(0,4):
 		a = max(data_dict, key=lambda x: data_dict[x][val] if data_dict[x][val] != 'NaN' else 0)
-		data_dict[a][val] = 'NaN'
-		counter +=1
-print 'Removed: ', counter, ' outliers.'
-
+		print '\t', a, ' = ', data_dict[a][val]
+		data_dict[a][val] = 0
+'''
 ### Task 3: Create new feature(s)
 for key in data_dict.keys():
-	email = data_dict[key]['email_address']
-	if email is not 'NaN':
-		if email in emails_dict:
-			data_dict[key]['from_count'] = emails_dict[email]['from']
-			data_dict[key]['to_count'] = emails_dict[email]['to']
-		else:
-			data_dict[key]['from_count'] = 'NaN'
-			data_dict[key]['to_count'] = 'NaN'
+	data_dict[key]['bonus/salary'] = \
+		float(data_dict[key]['bonus'])/float(data_dict[key]['salary']) \
+		if (data_dict[key]['salary'] != 0 and \
+			data_dict[key]['bonus'] != 'NaN' and \
+			data_dict[key]['salary'] != 'NaN') else 0
+
+	data_dict[key]['director_fees/deferral_payments'] = \
+		float(data_dict[key]['director_fees'])/float(data_dict[key]['deferral_payments']) \
+		if (data_dict[key]['deferral_payments'] != 0 and \
+			data_dict[key]['director_fees'] != 'NaN' and
+			data_dict[key]['deferral_payments'] != 'NaN') else 0
+
+	data_dict[key]['to_messages/deferral_payments'] = \
+		float(data_dict[key]['to_messages'])/float(data_dict[key]['deferral_payments']) \
+		if (data_dict[key]['deferral_payments'] != 0 and \
+			data_dict[key]['to_messages'] != 'NaN' and \
+			data_dict[key]['deferral_payments'] != 'NaN') else 0
+
+	data_dict[key]['from_messages/deferral_payments'] = \
+		float(data_dict[key]['from_messages'])/float(data_dict[key]['deferral_payments']) \
+		if (data_dict[key]['deferral_payments'] != 0 and \
+			data_dict[key]['from_messages'] != 'NaN' and \
+			data_dict[key]['deferral_payments'] != 'NaN') else 0
+	data_dict[key]['bonus/total_payments'] = \
+		float(data_dict[key]['bonus'])/float(data_dict[key]['total_payments']) \
+		if (data_dict[key]['total_payments'] != 0 and \
+			data_dict[key]['bonus'] != 'NaN' and \
+			data_dict[key]['total_payments'] != 'NaN') else 0
+
+	data_dict[key]['shared_receipt_with_poi/total_payments'] = \
+		float(data_dict[key]['shared_receipt_with_poi'])/float(data_dict[key]['total_payments']) \
+		if (data_dict[key]['total_payments'] != 0 and \
+			data_dict[key]['shared_receipt_with_poi'] != 'NaN' and \
+			data_dict[key]['total_payments'] != 'NaN') else 0
+	
+	data_dict[key]['exercised_stock_options/total_stock_value'] = \
+		float(data_dict[key]['exercised_stock_options'])/float(data_dict[key]['total_stock_value']) \
+		if (data_dict[key]['total_stock_value'] != 0 and \
+			data_dict[key]['exercised_stock_options'] != 'NaN' and \
+			data_dict[key]['total_stock_value'] != 'NaN') else 0
+	
+	data_dict[key]['restricted_stock/total_stock_value'] = \
+		float(data_dict[key]['restricted_stock'])/float(data_dict[key]['total_stock_value']) \
+		if (data_dict[key]['total_stock_value'] != 0 and \
+			data_dict[key]['restricted_stock'] != 'NaN' and \
+			data_dict[key]['total_stock_value'] != 'NaN') else 0
+	
+	data_dict[key]['from_this_person_to_poi/to_messages'] = \
+		float(data_dict[key]['from_this_person_to_poi'])/float(data_dict[key]['to_messages']) \
+		if (data_dict[key]['to_messages'] != 0 and \
+			data_dict[key]['from_this_person_to_poi'] != 'NaN' and \
+			data_dict[key]['to_messages'] != 'NaN') else 0
+	
+	data_dict[key]['shared_receipt_with_poi/to_messages'] = \
+		float(data_dict[key]['shared_receipt_with_poi'])/float(data_dict[key]['to_messages']) \
+		if (data_dict[key]['to_messages'] != 0 and \
+			data_dict[key]['shared_receipt_with_poi'] != 'NaN' and \
+			data_dict[key]['to_messages'] != 'NaN') else 0
+
 '''
-#poi_emails = poiEmails()
-#print 'GOT emails'
-#find_signature(emails, poi_emails)
+bonus/salary
+
+director_fees/deferral_payments
+to_messages/deferral_payments
+from_messages/deferral_payments
+
+bonus/total_payments
+shared_receipt_with_poi/total_payments
+
+exercised_stock_options/total_stock_value
+restricted_stock/total_stock_value
+
+from_this_person_to_poi/to_messages
+shared_receipt_with_poi/to_messages
+'''
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
@@ -111,7 +193,8 @@ X_train, X_test, y_train, y_test = cross_validation.train_test_split(features, l
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from sklearn.ensemble import RandomForestClassifier as RFC
 from sklearn.ensemble import AdaBoostClassifier as ABC
-clf = ABC(n_estimators= 5)#RFC(n_estimators= 3, criterion= 'entropy', max_features= None, min_samples_split= 20)
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 clf.fit(X_train, y_train) #fitting the data
 pred = clf.predict(X_test)
 acc = accuracy_score(pred, y_test)
@@ -130,6 +213,7 @@ print 'Precision: ', precision
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
+'''
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.1, random_state=42)
@@ -140,7 +224,6 @@ param_grid = {
          'max_features': ['sqrt', 'log2', None]
           }
 # for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
-from sklearn.ensemble import RandomForestClassifier as RFC
 #clf = GridSearchCV(RFC(), param_grid)
 
 clf = RFC(bootstrap=True, class_weight=None, criterion='gini',
@@ -163,6 +246,7 @@ precision = precision_score(labels_test, pred)
 print 'Accuracy:  ', acc
 print 'Recall:    ', recall
 print 'Precision: ', precision
+'''
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
--- Intro_to_Machine_Learning/ud120-projects-master/outliers/enron_outliers.py
+++ Intro_to_Machine_Learning/ud120-projects-master/outliers/enron_outliers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import pickle
 import sys
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import numpy
 
 sys.path.append("../tools/")
@@ -9,31 +9,127 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
-features = ["long_term_incentive", "bonus", "poi"]
+features = ['salary',
+			'deferral_payments',
+			'total_payments',
+			'loan_advances',
+			'bonus',
+			'restricted_stock_deferred',
+			'deferred_income',
+			'total_stock_value',
+			'expenses',
+			'exercised_stock_options',
+			'other',
+			'long_term_incentive',
+			'restricted_stock',
+			'director_fees',
+			'to_messages',
+			'from_poi_to_this_person',
+			'from_messages',
+			'from_this_person_to_poi',
+			'shared_receipt_with_poi',
+			'bonus/salary',
+			'director_fees/deferral_payments',
+			'to_messages/deferral_payments',
+			'from_messages/deferral_payments',
+			'bonus/total_payments',
+			'shared_receipt_with_poi/total_payments',
+			'exercised_stock_options/total_stock_value',
+			'restricted_stock/total_stock_value',
+			'from_this_person_to_poi/to_messages',
+			'shared_receipt_with_poi/to_messages',
+			'poi'
+			]
+print 'Done reading'
 data_dict.pop('TOTAL', 0 )
+
+### Task 2: Remove outliers
+keys_outliers = ['SKILLING JEFFREY K',
+				'LAY KENNETH L',
+				'FREVERT MARK A',
+				'BHATNAGAR SANJAY',
+				'LAVORATO JOHN J',
+				'DELAINEY DAVID W',
+				'BELDEN TIMOTHY N',
+				'MARTIN AMANDA K',
+				'DERRICK JR. JAMES V',
+				'PICKERING MARK R'
+				]
+for val in keys_outliers:
+	del data_dict[val]
+for key in data_dict.keys():
+	data_dict[key]['bonus/salary'] = \
+		float(data_dict[key]['bonus'])/float(data_dict[key]['salary']) \
+		if (data_dict[key]['salary'] != 0 and \
+			data_dict[key]['bonus'] != 'NaN' and \
+			data_dict[key]['salary'] != 'NaN') else 0
+
+	data_dict[key]['director_fees/deferral_payments'] = \
+		float(data_dict[key]['director_fees'])/float(data_dict[key]['deferral_payments']) \
+		if (data_dict[key]['deferral_payments'] != 0 and \
+			data_dict[key]['director_fees'] != 'NaN' and
+			data_dict[key]['deferral_payments'] != 'NaN') else 0
+
+	data_dict[key]['to_messages/deferral_payments'] = \
+		float(data_dict[key]['to_messages'])/float(data_dict[key]['deferral_payments']) \
+		if (data_dict[key]['deferral_payments'] != 0 and \
+			data_dict[key]['to_messages'] != 'NaN' and \
+			data_dict[key]['deferral_payments'] != 'NaN') else 0
+
+	data_dict[key]['from_messages/deferral_payments'] = \
+		float(data_dict[key]['from_messages'])/float(data_dict[key]['deferral_payments']) \
+		if (data_dict[key]['deferral_payments'] != 0 and \
+			data_dict[key]['from_messages'] != 'NaN' and \
+			data_dict[key]['deferral_payments'] != 'NaN') else 0
+
+	data_dict[key]['bonus/total_payments'] = \
+		float(data_dict[key]['bonus'])/float(data_dict[key]['total_payments']) \
+		if (data_dict[key]['total_payments'] != 0 and \
+			data_dict[key]['bonus'] != 'NaN' and \
+			data_dict[key]['total_payments'] != 'NaN') else 0
+
+	data_dict[key]['shared_receipt_with_poi/total_payments'] = \
+		float(data_dict[key]['shared_receipt_with_poi'])/float(data_dict[key]['total_payments']) \
+		if (data_dict[key]['total_payments'] != 0 and \
+			data_dict[key]['shared_receipt_with_poi'] != 'NaN' and \
+			data_dict[key]['total_payments'] != 'NaN') else 0
+	
+	data_dict[key]['exercised_stock_options/total_stock_value'] = \
+		float(data_dict[key]['exercised_stock_options'])/float(data_dict[key]['total_stock_value']) \
+		if (data_dict[key]['total_stock_value'] != 0 and \
+			data_dict[key]['exercised_stock_options'] != 'NaN' and \
+			data_dict[key]['total_stock_value'] != 'NaN') else 0
+	
+	data_dict[key]['restricted_stock/total_stock_value'] = \
+		float(data_dict[key]['restricted_stock'])/float(data_dict[key]['total_stock_value']) \
+		if (data_dict[key]['total_stock_value'] != 0 and \
+			data_dict[key]['restricted_stock'] != 'NaN' and \
+			data_dict[key]['total_stock_value'] != 'NaN') else 0
+	
+	data_dict[key]['from_this_person_to_poi/to_messages'] = \
+		float(data_dict[key]['from_this_person_to_poi'])/float(data_dict[key]['to_messages']) \
+		if (data_dict[key]['to_messages'] != 0 and \
+			data_dict[key]['from_this_person_to_poi'] != 'NaN' and \
+			data_dict[key]['to_messages'] != 'NaN') else 0
+	
+	data_dict[key]['shared_receipt_with_poi/to_messages'] = \
+		float(data_dict[key]['shared_receipt_with_poi'])/float(data_dict[key]['to_messages']) \
+		if (data_dict[key]['to_messages'] != 0 and \
+			data_dict[key]['shared_receipt_with_poi'] != 'NaN' and \
+			data_dict[key]['to_messages'] != 'NaN') else 0
+
 data = featureFormat(data_dict, features)
-from outlier_cleaner import outlierCleaner
 
-'''
-sals = data[0:,[0]]
-outlier1 = sals.max()
-print outlier1
-ind = numpy.argmax(sals)
-temp = numpy.delete(sals, ind)
-outlier2 = numpy.max(temp)
-
-for item in data_dict:
-	for k, v in data_dict[item].items():
-		if v == outlier1 or v == outlier2:
-			print 'OUTLIER: \t', item
-			print k, v
-'''
 ### your code below
-for point in data:
-    poi = point[0]
-    bonus = point[1]
-    matplotlib.pyplot.scatter( poi, bonus , color= ('blue' if point[2] == 1 else 'red'))
-
-matplotlib.pyplot.xlabel("deferred_income")
-matplotlib.pyplot.ylabel("bonus")
-matplotlib.pyplot.show()
+for i in range(19, 29):
+	plt.figure(figsize=(30,20))
+	for point in data:
+		x = point[i]
+		y = point[i]
+		plt.scatter( x, y , color= ('blue' if point[19] == 1 else 'red'))
+	plt.xlabel(features[i])
+	plt.ylabel(features[i])
+	name = str(2222) + features[i]
+	name = name.replace('/', '--')
+	plt.savefig(name + ".png")
+	#plt.show()
